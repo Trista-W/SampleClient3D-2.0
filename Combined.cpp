be65a4201ae5067a188bf3f7a2644664edd1262e
Trista <trista.0w3@gmail.com>
2018-10-17 12:08:11 -0400

    problem is not its position but the shader for sure

--- Combined.cpp
+++ Combined.cpp
@@ -532,45 +532,12 @@ void RenderOGLScene()
     OpenGLDrawingFunctions::DrawGrid();
 	///////////////////////////////////////////////////////////////////////////
 
-
-
-	// Enable depth test
-	glEnable(GL_DEPTH_TEST);
-	// Accept fragment if it is closer to the camera than the former one
-	glDepthFunc(GL_LESS);
-
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
-
-	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader");
-
-	// Get a handle for our "MVP" uniform
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-
-	// Projection matrix : 45?Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(40.0f), 1136.0f / 534.0f, 1.0f, 10000.0f);
-	// Camera matrix
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(0, 3000, 5000), // Camera is at (0,3,5), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-	);
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
-
-	// Load the texture using any two methods
+	//GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader");
 	GLuint Texture = loadBMP_custom("Texture.bmp");
-	//GLuint Texture = loadDDS("uvtemplate.DDS");
-
-	// Get a handle for our "myTextureSampler" uniform
-	GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
-
-	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+	//GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 	static const GLfloat g_vertex_buffer_data[] = {
 
 
@@ -584,7 +551,6 @@ void RenderOGLScene()
 
 
 	};
-
 	// Two UV coordinatesfor each vertex. They were created with Blender.
 	static const GLfloat g_uv_buffer_data[] = {
 
@@ -604,37 +570,19 @@ void RenderOGLScene()
 
 
 	};
-
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
 	GLuint uvbuffer;
-	glGenBuffers(1, &uvbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+    glGenBuffers(1, &uvbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
 
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-
-	
-	// Clear the screen
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Use our shader
-	//glUseProgram(programID);
-
-	// Send our transformation to the currently bound shader, 
-	// in the "MVP" uniform
-	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-	// Bind our texture in Texture Unit 0
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, Texture);
 	// Set our "myTextureSampler" sampler to use Texture Unit 0
-	glUniform1i(TextureID, 0);
+
 
 	// 1rst attribute buffer : vertices
 	glEnableVertexAttribArray(0);
@@ -647,8 +595,7 @@ void RenderOGLScene()
 		0,                  // stride
 		(void*)0            // array buffer offset
 	);
-
-	// 2nd attribute buffer : UVs
+	 //2nd attribute buffer : UVs
 	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glVertexAttribPointer(
@@ -659,7 +606,6 @@ void RenderOGLScene()
 		0,                                // stride
 		(void*)0                          // array buffer offset
 	);
-
 	// Draw the triangle !
 	glDrawArrays(GL_TRIANGLES, 0, 4 * 3); // 12*3 indices starting at 0 -> 12 triangles
 
@@ -667,15 +613,211 @@ void RenderOGLScene()
 	glDisableVertexAttribArray(0);
 	glDisableVertexAttribArray(1);
 
+	//// Enable depth test
+	//glEnable(GL_DEPTH_TEST);
+ //   // Accept fragment if it is closer to the camera than the former one
+	//glDepthFunc(GL_LESS);
+
+	//GLuint VertexArrayID;
+	//glGenVertexArrays(1, &VertexArrayID);
+	//glBindVertexArray(VertexArrayID);
+
+	//// Create and compile our GLSL program from the shaders
+	//GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader");
+
+	//// Get a handle for our "MVP" uniform
+	//GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	//// Projection matrix : 45?Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	//glm::mat4 Projection = glm::perspective(glm::radians(40.0f),  1136.0f/534.0f, 1.0f, 10000.0f);   //-15.0f
+	//// Camera matrix
+	//glm::mat4 View = glm::lookAt(
+	//	glm::vec3(0, 1000, 3000), // Camera is at (0,3,5), in World Space
+	//	glm::vec3(0, 0, 0), // and looks at the origin
+	//	glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+	//);
+	//// Model matrix : an identity matrix (model will be at the origin)
+	//glm::mat4 Model = glm::mat4(1.0f);
+	//// Our ModelViewProjection : multiplication of our 3 matrices
+	//glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+	//// Load the texture using any two methods
+	//GLuint Texture = loadBMP_custom("Texture.bmp");
+
+
+	//// Get a handle for our "myTextureSampler" uniform
+	//GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
+
+	////Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+	////A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+	//static const GLfloat g_vertex_buffer_data[] = {
+
+
+	//	 1000.0f,0.0f, 1000.0f,
+	//	-1000.0f,0.0f,-1000.0f,
+	//	 1000.0f,0.0f,-1000.0f,
+
+	//	 1000.0f,0.0f, 1000.0f,
+	//	-1000.0f,0.0f, 1000.0f,
+	//	-1000.0f,0.0f,-1000.0f,
+
+
+	//};
+
+	//// Two UV coordinatesfor each vertex. They were created with Blender.
+	//static const GLfloat g_uv_buffer_data[] = {
+
+
+	//	//1.0f, 1.0f,
+	//	//0.0f, 0.0f,
+	//	//1.0f, 0.0f,
+	//	//1.0f, 1.0f,
+	//	//0.0f, 1.0f,
+	//	//0.0f, 0.0f
+	//	0.3f, 0.3f,
+	//	0.0f, 0.0f,
+	//	0.3f, 0.0f,
+	//	0.3f, 0.3f,
+	//	0.0f, 0.3f,
+	//	0.0f, 0.0f
+
+
+	//};
+
+	//GLuint vertexbuffer;
+	//glGenBuffers(1, &vertexbuffer);
+	//glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	//GLuint uvbuffer;
+	//glGenBuffers(1, &uvbuffer);
+	//glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
+
+	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	//glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+	////glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+
+	//
+	//// Clear the screen
+	////glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+ //   // Use our shader
+	//glUseProgram(programID);
+
+	//// Send our transformation to the currently bound shader, 
+	//// in the "MVP" uniform
+ //   glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+	//// Bind our texture in Texture Unit 0
+	//glActiveTexture(GL_TEXTURE0);
+	//glBindTexture(GL_TEXTURE_2D, Texture);
+	//// Set our "myTextureSampler" sampler to use Texture Unit 0
+	//glUniform1i(TextureID, 0);
+	//glUniform1i(TextureID, 1);
+
+	//// 1rst attribute buffer : vertices
+	//glEnableVertexAttribArray(0);
+	//glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	//glVertexAttribPointer(
+	//	0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+	//	3,                  // size
+	//	GL_FLOAT,           // type
+	//	GL_FALSE,           // normalized?
+	//	0,                  // stride
+	//	(void*)0            // array buffer offset
+	//);
+
+	// //2nd attribute buffer : UVs
+	//glEnableVertexAttribArray(1);
+	//glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+	//glVertexAttribPointer(
+	//	1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+	//	2,                                // size : U+V => 2
+	//	GL_FLOAT,                         // type
+	//	GL_FALSE,                         // normalized?
+	//	0,                                // stride
+	//	(void*)0                          // array buffer offset
+	//);
+
+	//// Draw the triangle !
+	//glDrawArrays(GL_TRIANGLES, 0, 4 * 3); // 12*3 indices starting at 0 -> 12 triangles
+
+
+	//glDisableVertexAttribArray(0);
+	//glDisableVertexAttribArray(1);
+
 
 
 
 	// Cleanup VBO and shader
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteBuffers(1, &uvbuffer);
-	glDeleteProgram(programID);
-	glDeleteTextures(1, &Texture);
+	//glDeleteTextures(1, &Texture);
 	glDeleteVertexArrays(1, &VertexArrayID);
+
+
+	
+
+
+	//// Build and compile our shader program
+ //   GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "TextureFragmentShader.fragmentshader");
+
+
+	//// Set up vertex data (and buffer(s)) and attribute pointers
+	//GLfloat vertices[] = {
+	//	// Positions          // Colors           // Texture Coords
+	//	  1000.0f,0.0f, 1000.0f,    1.0f, 1.0f, // Top Right
+	//	 -1000.0f,0.0f,-1000.0f,    0.0f, 0.0f, // Bottom Right
+	//	  1000.0f,0.0f,-1000.0f,    1.0f, 0.0f, // Bottom Left
+	//	  1000.0f,0.0f, 1000.0f,    1.0f, 1.0f  // Top Left 
+	//	 -1000.0f,0.0f, 1000.0f,    0.0f, 1.0f,
+	//	 -1000.0f,0.0f,-1000.0f,    0.0f, 0.0f
+
+	//};
+	//GLuint indices[] = {  // Note that we start from 0!
+	//	0, 1, 3, // First Triangle
+	//	1, 2, 3  // Second Triangle
+	//};
+	//GLuint VBO, VAO, EBO;
+	//glGenVertexArrays(1, &VAO);
+	//glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
+
+	//glBindVertexArray(VAO);
+
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//// Position attribute
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+	//glEnableVertexAttribArray(0);
+	////// Color attribute
+	////glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	////glEnableVertexAttribArray(1);
+	//// TexCoord attribute
+	//glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+	//glEnableVertexAttribArray(2);
+
+	//glBindVertexArray(0); // Unbind VAO
+
+	//GLuint Texture = loadBMP_custom("Texture.bmp");
+	//
+	//glUseProgram(programID);
+
+
+	//// Properly de-allocate all resources once they've outlived their purpose
+	//glDeleteVertexArrays(1, &VAO);
+	//glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
+	
+
+
+
+
 
 
 
